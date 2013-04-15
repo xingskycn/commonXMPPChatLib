675df9f5c4d083caff285f38dfbe835369d3c2e1
zuoyunlong <decazuk@gmail.com>
2013-04-15 17:34:16 +0800

    Initial Commit

---
+++ KechengIphoneChatLib/KechengIphoneChatLib.h
@@ -0,0 +1,13 @@
+//
+//  KechengIphoneChatLib.h
+//  KechengIphoneChatLib
+//
+//  Created by zuoyl on 4/15/13.
+//  Copyright (c) 2013 zuoyl. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface KechengIphoneChatLib : NSObject
+
+@end
